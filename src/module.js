de36010c90d524648e3c7ed3050ad61e3fd5bcba
Valentina Maldonado <c.valentinamm@gmail.com>
2020-06-23 18:28:46 -0400

    renamed main file

--- src/module.js
+++
@@ -1,8 +0,0 @@
-import * as angular from 'angular';
-import { view1 } from './view1.js';
-import { AppRoutes } from './view1.js';
-import './styles.scss';
-
-const app = angular.module('wisboo', [])
-  .config(AppRoutes);
-  .component('view1', view1);
